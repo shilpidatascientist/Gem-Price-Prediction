48f7554d9a13d60a31509be3d12fa5ecfb4a593a
shilpia22 <shilpia22@iimb.ac.in>
2024-01-01 18:05:37 +0530

    first commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="Shilpi",
+    author_email="shilpia22@iimb.ac.in",
     packages=find_packages(),
     install_requires=[],
 )
